d731532d9045ba8c4c01e01e78e61874bcc6f4b1
Chris Cowperthwait <ccowperthwait@gmail.com>
2020-02-18 20:45:31 -0700

    Checking in package-info.

---
+++ from-intellij/src/main/java/guru/springframework/intellij/HelloFromJetbrains.java
@@ -0,0 +1,11 @@
+package guru.springframework.intellij;
+
+/**
+ * Created by ccowperthwait on 2020-02-18
+ */
+public class HelloFromJetbrains {
+
+    public String sayHello(){
+        return "Hello from Jetbrains!";
+    }
+}
